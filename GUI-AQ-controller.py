2d1caaa5e4fad6eeb8406623103f2ed442b41e64
puchtuningproduction@gmail.com <45675535+puchtuning@users.noreply.github.com>
2020-01-17 18:39:49 +0100

    Erste Programmierung

---
+++ GUI-AQ-controller.py
@@ -0,0 +1,151 @@
+from tkinter import Tk,Label,Entry,Button,Spinbox
+from tkinter import ttk
+from tkinter import messagebox
+
+import xml.etree.cElementTree as ET
+import time
+
+
+
+#---Time Function
+timestamp = time.strftime("%d.%m.%Y %H:%M")
+
+
+#--Check for Blanks
+def is_not_blank(mystring,length):
+    if(len(mystring) == length):
+        error = "none"
+    else:
+        raise ValueError('No Value specified') #Erstellt einen Error der durch die try Funktion gewertet erden kann
+    
+
+#---Click Funktionen
+def clicked():
+    res = "Entry: " + txt1.get() #--Liest den Text aus dem txt1 aus
+    lbl2.configure(text=res)
+
+def clickecancel():
+    window.destroy()
+
+def clickedadmin():
+    aq_main_light_on = txtadminl1.get() #--Liest den Text aus dem txt1 aus
+    aq_main_light_off = txtadminl2.get()
+    aq_co2_on = txtadminc1.get()
+    aq_co2_off = txtadminc2.get()
+    aq_temp = spinadmint1.get()
+
+    
+    try:
+        is_not_blank(aq_main_light_on,5)
+        is_not_blank(aq_main_light_off,5)
+        is_not_blank(aq_co2_on,5)
+        is_not_blank(aq_co2_off,5)
+        is_not_blank(aq_temp,2)
+
+
+
+        root = ET.Element("xml")
+        doc = ET.SubElement(root, "controller-input")
+        ET.SubElement(doc, "aq_main_light_on").text = aq_main_light_on
+        ET.SubElement(doc, "aq_main_light_off").text = aq_main_light_off
+        ET.SubElement(doc, "aq_co2_on").text = aq_co2_on
+        ET.SubElement(doc, "aq_co2_off").text = aq_co2_off
+        ET.SubElement(doc, "aq_temp").text = aq_temp
+        ET.SubElement(doc, "written_at").text = timestamp
+
+
+        tree = ET.ElementTree(root)
+    
+        tree.write("controller-input.xml")
+        print("Values written")
+        messagebox.showinfo('Action successfull','Values written successfully') #shows info message
+
+    except ValueError:
+        print("Values missing")
+        messagebox.showerror('Error: Falsche Werte', 'Eingabe Überprüfen')  #shows warning message
+
+    
+ 
+window = Tk() #--Startet Tkinter als window
+
+
+
+ 
+window.title("Aquarium Controller GUI") #--Titel des Fensters
+window.geometry('700x600') #--Definiert die Fenstergroesse
+
+tab_control = ttk.Notebook(window)
+ 
+tab1 = ttk.Frame(tab_control) 
+tab_control.add(tab1, text='Dashboard') 
+tab_control.pack(expand=1, fill='both')
+
+tab2 = ttk.Frame(tab_control)
+tab_control.add(tab2, text='Admin')
+tab_control.pack(expand=1, fill='both')
+
+##--- Tab Dashboard
+
+lbl1 = Label(tab1, text="Aquarium Controller Dashboard" ,font=("Arial Bold", 20)) #--Definiert ein Textlable
+lbl1.grid(column=0, row=0) #--Definiert die Position des Textlabels
+lbl2 = Label(tab1, text="" ,font=("Arial Bold", 20))
+lbl2.grid(column=0, row=2)
+
+txt1 = Entry(tab1, width=10, state='normal') #--Defiiniert ein Eingabefeld/ mit State kann man das Texfeld aktivieren oder deaktivieren
+txt1.focus() #--Setzt den Curser direkt in das Texfeld
+txt1.grid(column=0, row=1)
+
+
+btn1 = Button(tab1, text="Datenladen", bg="gray", fg="White", command=clicked) #--Definiert ein Button
+btn1.grid(column=1, row=2) #--Definiert die Position des Buttons
+
+##--- Tab Admin
+lbladmin1 = Label(tab2, text="Aquarium Controller Admin", font=("Arial Bold", 20))
+
+lbladminl1 = Label(tab2, text="Licht", font=("Arial", 10))
+lbladminl2 = Label(tab2, text="Einschaltzeit (z.B. 08:00):", font=("Arial", 10))
+lbladminl3 = Label(tab2, text="Ausschaltzeit (z.B. 22:00):", font=("Arial", 10))
+
+lbladminc1 = Label(tab2, text="CO2", font=("Arial", 10))
+lbladminc2 = Label(tab2, text="Einschaltzeit (z.B. 08:00):", font=("Arial", 10))
+lbladminc3 = Label(tab2, text="Ausschaltzeit (z.B. 22:00):", font=("Arial", 10))
+
+lbladmint1 = Label(tab2, text="Temperatur", font=("Arial", 10))
+lbladmint2 = Label(tab2, text="Wunschtemperatur (z.B. 25):", font=("Arial", 10))
+
+txtadminl1 = Entry(tab2, width=15, state='normal')
+txtadminl2 = Entry(tab2, width=15, state='normal')
+
+txtadminc1 = Entry(tab2, width=15, state='normal')
+txtadminc2 = Entry(tab2, width=15, state='normal')
+
+spinadmint1 = Spinbox(tab2, from_=15, to=40, width=5)
+
+
+lbladmin1.grid(column=0, row=0)
+
+lbladminl1.grid(column=0, row=1)
+lbladminl2.grid(column=0, row=2)
+txtadminl1.grid(column=1, row=2)
+lbladminl3.grid(column=0, row=3)
+txtadminl2.grid(column=1, row=3)
+
+lbladminc1.grid(column=0, row=4)
+lbladminc2.grid(column=0, row=5)
+txtadminc1.grid(column=1, row=5)
+lbladminc3.grid(column=0, row=6)
+txtadminc2.grid(column=1, row=6)
+
+lbladmint1.grid(column=0, row=7)
+lbladmint2.grid(column=0, row=8)
+spinadmint1.grid(column=1, row=8)
+
+btnadmin1 = Button(tab2, text="Config File Schreiben", bg="gray", fg="White", command=clickedadmin) #--Definiert ein Button
+btnadmin1.grid(column=1, row=10) #--Definiert die Position des Buttons
+
+btnadmin2 = Button(tab2, text="Cancel", bg="gray", fg="White", command=clickecancel) #--Definiert ein Button
+btnadmin2.grid(column=2, row=10) #--Definiert die Position des Buttons
+
+
+
+window.mainloop()
---
+++ Server-AQ-controller.py
@@ -0,0 +1,119 @@
+import time
+import os
+import xml.etree.cElementTree as ET
+import xml.dom.minidom as dom
+import mysql.connector
+
+
+##---Write-Frequency
+##Default value is 5 Seconds
+sleeptime = 5
+
+##-----Controller-Values
+Controller_ID = "Raps02test"
+
+##-----mysql-connection infos
+mydb = mysql.connector.connect(
+  host="192.168.0.103",
+  user="aq_controller",
+  passwd="6010Kriens$",
+  database="Aquarium"
+)
+
+
+
+
+##---Functions Start
+
+##---Controller Input XML Function
+
+def load_controller_input(xmlnode):
+    print("Read controller input file")
+
+    xmlinput = dom.parse("controller-input.xml")
+
+    for eintrag in xmlinput.firstChild.childNodes:
+        if eintrag.nodeName == "controller-input":
+            for knoten in eintrag.childNodes:
+                if(knoten.nodeName == xmlnode):
+                    print(xmlnode + ": " + knoten.firstChild.data.strip())
+                    xmlvalue = knoten.firstChild.data.strip()
+                
+    return(str(xmlvalue))    
+
+##--Functions Ende
+
+
+aq_main_light_on = load_controller_input("aq_main_light_on")
+aq_main_light_off = load_controller_input("aq_main_light_off")
+aq_co2_on = load_controller_input("aq_co2_on")
+aq_co2_off = load_controller_input("aq_co2_off")
+aq_temp = load_controller_input("aq_temp")
+
+
+
+while True:
+
+
+
+##---Get Times
+    daytime = time.strftime("%H:%M")
+    date = time.strftime("%d.%m.%Y")
+    fulltime = time.strftime("%d.%m.%Y %H:%M:%S")
+
+##---Light Switching
+
+    if(daytime >= aq_main_light_on and daytime <= aq_main_light_off):
+        aq_main_light_status = "On"
+    else:
+        aq_main_light_status = "Off"
+
+##---CO2 Switching
+
+    if(daytime >= aq_co2_on and daytime <= aq_co2_off):
+        aq_co2_status = "On"
+    else:
+        aq_co2_status = "Off"
+
+
+
+
+
+
+
+
+
+
+##---Output
+##--Terminal output
+    print("---Controller Values---")
+    print("--Date and Time--")
+    print(date, daytime, ":Date and Daytime")
+    print(fulltime, ":Fulltime")
+    print("--Light--")
+    print("Target time light on", aq_main_light_on,"and licht off", aq_main_light_off)
+    print("Light status: ", aq_main_light_status)
+
+    print("--CO2--")
+    print("Target time CO2 on", aq_co2_on, "and CO2 off", aq_co2_off)
+    print("CO2 status: ", aq_co2_status)
+
+    print("--Temprature--")
+    print("Target temprature:", aq_temp)
+
+##--SQL output
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO aq_controller (Controller_ID, aq_timestamp, aq_mainlight, aq_temp, aq_heater) VALUES (%s, %s, %s, %s, %s)"
+    val = (Controller_ID,  fulltime, aq_main_light_status, "0", "Off")
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+
+
+##---Delay
+    time.sleep(sleeptime)
+
+##---Beauty-Command
+    os.system('clear') #Disabele for Debug
