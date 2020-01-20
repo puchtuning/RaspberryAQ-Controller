47e8461cd7c938b9acb12ec3c3f34466f4716457
puchtuningproduction@gmail.com <45675535+puchtuning@users.noreply.github.com>
2020-01-20 22:19:01 +0100

    - Changing the GUI

--- GUI-AQ-controller.py
+++ GUI-AQ-controller.py
@@ -1,25 +1,44 @@
-from tkinter import Tk, Label, Entry, Button, Spinbox
+import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-import os
+
 import time
-import logging  # -Enables to write Logfiles
-import json  # -To write/read the data Files
+import json
+import logging
 
 
-# ---Time Function
+WIDTH = 800
+HEIGHT = 600
+
+H1 = ("Roboto", 24)
+H2 = ("Roboto", 18)
+
+
+BGFRAME = '#00DDFF'
+
+
+aq_main_light_on = ""
+aq_main_light_off = ""
+aq_co2_on = ""
+aq_co2_off = ""
+aq_temp = ""
+
+
+
+#--- Time Function
 timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
 logtime = time.strftime("%Y-%m-%d")
 
-
-# ---Logging
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-                    filename="log/" + logtime + "_Server-RaspberryAQ.log", level=logging.INFO)
+#--- Logging
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename="log/" + logtime + "_GUI-RaspberryAQ.log", level=logging.INFO)
 logging.info('GUI-RaspberryAQ Started!')
 
-# ---Check for Blanks
 
+#--- Click Funktionen
+def clickecancel():
+    app.destroy()
 
+#--- Check for Blanks
 def is_not_blank(mystring, length):
     if(len(mystring) == length):
         print("")
@@ -28,144 +47,169 @@ def is_not_blank(mystring, length):
         raise ValueError('No Value specified')
 
 
-# ---Click Funktionen
-
-
-
-def clickecancel():
-    logging.info('Closing RasperryAQ-GUI')
-    window.destroy()
-
-
+    
+#--- Starting Admin menu
 def clickedadmin():
-    aq_main_light_on = txtadminl1.get()  # --Liest den Text aus dem txt1 aus
-    aq_main_light_off = txtadminl2.get()
-    aq_co2_on = txtadminc1.get()
-    aq_co2_off = txtadminc2.get()
-    aq_temp = spinadmint1.get()
+
+    def saveconfigfile():
+        aq_main_light_on = txtadminl1.get()  # --Liest den Text aus dem txt1 aus
+        aq_main_light_off = txtadminl2.get()
+        aq_co2_on = txtadminc1.get()
+        aq_co2_off = txtadminc2.get()
+        aq_temp = spinadmint1.get()
+        
+
+        try:
+            is_not_blank(aq_main_light_on, 5)
+            is_not_blank(aq_main_light_off, 5)
+            is_not_blank(aq_co2_on, 5)
+            is_not_blank(aq_co2_off, 5)
+            is_not_blank(aq_temp, 2)
+
+            timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
+
+            # --Initialize JSON
+            controllerinput = {}
+
+            controllerinput["Controller-input"] = {  # Construct the input JSON file
+                "timestamp": timestamp,
+                "aq_main_light_on": aq_main_light_on,
+                "aq_main_light_off": aq_main_light_off,
+                "aq_co2_on": aq_co2_on,
+                "aq_co2_off": aq_co2_off,
+                "aq_temp": aq_temp
+            }
+
+            with open("data/_controller-input.json", 'w') as f:
+                json.dump(controllerinput, f)
+
+            print("Values written")
+            logging.info('Values have been saved in JSON file')
+            # shows info message
+            messagebox.showinfo('Action successfull',
+                                'Values written successfully')
+
+        except ValueError:
+            print("Values missing")
+            logging.warning('Values could not be written')
+            # shows warning message
+            messagebox.showerror('Error: Falsche Werte', 'Eingabe Überprüfen')
+
+    appadmin = tk.Toplevel()
+    appadmin.title("Admin GUI")
+
+    canvas = tk.Canvas(appadmin, height=HEIGHT, width=WIDTH, bg='gray')
+    canvas.pack()
+    header = tk.Frame(appadmin, bg=BGFRAME, bd=5)
+    header.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.1)
+
+    title = tk.Label(header,anchor='center', text="RaspberryAQ-controller", bg=BGFRAME, bd=0, font=H1)
+    title.place(relwidth=1, relheight=1)
+    
+    ##--- Values
+
+    frameval = tk.Frame(appadmin, bg=BGFRAME, bd=5)
+    frameval.place(relx=0.1, rely=0.2, relwidth=0.8, relheight=0.6)
+
+    frameleft = tk.Frame(frameval, bg=BGFRAME)
+    frameleft.pack(side='left', fill='both', padx=5, pady=5, expand=True)
+
+    frameright = tk.Frame(frameval, bg=BGFRAME)
+    frameright.pack(side='right', fill='both', padx=5, pady=5, expand=True)
     
 
-    try:
-        is_not_blank(aq_main_light_on, 5)
-        is_not_blank(aq_main_light_off, 5)
-        is_not_blank(aq_co2_on, 5)
-        is_not_blank(aq_co2_off, 5)
-        is_not_blank(aq_temp, 2)
+    tk.Label(frameleft, anchor='w', text="Licht", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
+    tk.Label(frameleft, anchor='w', text="Einschaltzeit (z.B. 08:00):", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
+    tk.Label(frameleft, anchor='w', text="Ausschaltzeit (z.B. 22:00):", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
 
-        timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
+    tk.Label(frameleft, anchor='w', text="CO2", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
+    tk.Label(frameleft, anchor='w', text="Einschaltzeit (z.B. 08:00):", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
+    tk.Label(frameleft, anchor='w', text="Ausschaltzeit (z.B. 22:00):", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
 
-        # --Initialize JSON
-        controllerinput = {}
+    tk.Label(frameleft, anchor='w', text="Temperatur", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
+    tk.Label(frameleft, anchor='w', text="Wunschtemperatur (z.B. 25):", bg=BGFRAME, font=H2).pack(padx=5, pady=5, fill='x')
 
-        controllerinput["Controller-input"] = {  # Construct the input JSON file
-            "timestamp": timestamp,
-            "aq_main_light_on": aq_main_light_on,
-            "aq_main_light_off": aq_main_light_off,
-            "aq_co2_on": aq_co2_on,
-            "aq_co2_off": aq_co2_off,
-            "aq_temp": aq_temp
-        }
+    filler1 = tk.Label(frameright, text="", bg=BGFRAME, font=("Roboto", 18))
+    txtadminl1 = tk.Entry(frameright, width=15, state='normal', font=H2)
+    txtadminl2 = tk.Entry(frameright, width=15, state='normal', font=H2)
 
-        with open("data/_controller-input.json", 'w') as f:
-            json.dump(controllerinput, f)
+    filler2 = tk.Label(frameright, text="", bg=BGFRAME, font=("Roboto", 20))
+    txtadminc1 = tk.Entry(frameright, width=15, state='normal', font=H2)
+    txtadminc2 = tk.Entry(frameright, width=15, state='normal', font=H2)
 
-        print("Values written")
-        logging.info('Values have been saved in JSON file')
-        # shows info message
-        messagebox.showinfo('Action successfull',
-                            'Values written successfully')
+    filler3 = tk.Label(frameright, text="", bg=BGFRAME, font=("Roboto", 19))
+    spinadmint1 = tk.Spinbox(frameright, from_=15, to=40, width=5, font=H2)
 
-    except ValueError:
-        print("Values missing")
-        logging.warning('Values could not be written')
-        # shows warning message
-        messagebox.showerror('Error: Falsche Werte', 'Eingabe Überprüfen')
+    filler1.pack(padx=5, pady=5) #Filler
+    txtadminl1.pack(padx=5, pady=5, fill='x')
+    txtadminl2.pack(padx=5, pady=5, fill='x')
+    filler2.pack(padx=5, pady=5) #Filler
+    txtadminc1.pack(padx=5, pady=5, fill='x')
+    txtadminc2.pack(padx=5, pady=5, fill='x')
+    filler3.pack(padx=5, pady=5) #Filler
+    spinadmint1.pack(padx=5, pady=5, fill='x')
 
+    ##--- Buttons
 
-window = Tk()  # --Startet Tkinter als window
+    framebtn = tk.Frame(appadmin, bg=BGFRAME, bd=5)
+    framebtn.place(relx=0.1, rely=0.85, relwidth=0.8, relheight=0.1)
 
+    btnadmin = tk.Button(framebtn, text="Save configfile", bg="gray", fg="White", font=H2, command=saveconfigfile)  # --Definiert ein Button
+    btnadmin.place(relwidth=0.49, relheight=1)  # --Definiert die Position des Buttons
 
-window.title("RaspberryAQ GUI")  # --Titel des Fensters
-window.geometry('600x500')  # --Definiert die Fenstergroesse
-
-tab_control = ttk.Notebook(window)
-
-tab1 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='Dashboard')
-tab_control.pack(expand=1, fill='both')
-
-tab2 = ttk.Frame(tab_control)
-tab_control.add(tab2, text='Admin')
-tab_control.pack(expand=1, fill='both')
-
-# --- Tab Dashboard
-
-lbl1 = Label(tab1, text="RaspberryAQ Dashboard", font=("Arial Bold", 20))  # --Definiert ein Textlable
-lbl1.grid(column=0, row=0)  # --Definiert die Position des Textlabels
-lbl2 = Label(tab1, text="Coming soon.....:)", font=("Arial Bold", 10))
-lbl2.grid(column=0, row=2)
+    btncancel = tk.Button(framebtn, text="Back", bg="gray", fg="White", font=H2, command= lambda: appadmin.destroy())  # --Definiert ein Button
+    btncancel.place(relx=0.5, relwidth=0.49, relheight=1)  # --Definiert die Position des Buttons
+        
+    
+    
+    
+    
+    appadmin.mainloop()
 
 
 
+##-- Starting the GUI
+app = tk.Tk()
+
+app.title("RaspberryAQ GUI")
+
+canvas = tk.Canvas(app, height=HEIGHT, width=WIDTH)
+bgimage = tk.PhotoImage(file='_img/background.png')
+bglabel = tk.Label(app, image=bgimage)
+bglabel.place(bordermode='outside', relwidth=1, relheight=1)
 
 
-
-# --- Tab Admin
-lbladmin1 = Label(tab2, text="RaspberryAQ Admin", font=("Arial Bold", 20))
-
-lbladminl1 = Label(tab2, text="Licht", font=("Arial", 10))
-lbladminl2 = Label(tab2, text="Einschaltzeit (z.B. 08:00):",
-                   font=("Arial", 10))
-lbladminl3 = Label(tab2, text="Ausschaltzeit (z.B. 22:00):",
-                   font=("Arial", 10))
-
-lbladminc1 = Label(tab2, text="CO2", font=("Arial", 10))
-lbladminc2 = Label(tab2, text="Einschaltzeit (z.B. 08:00):",
-                   font=("Arial", 10))
-lbladminc3 = Label(tab2, text="Ausschaltzeit (z.B. 22:00):",
-                   font=("Arial", 10))
-
-lbladmint1 = Label(tab2, text="Temperatur", font=("Arial", 10))
-lbladmint2 = Label(tab2, text="Wunschtemperatur (z.B. 25):",
-                   font=("Arial", 10))
-
-txtadminl1 = Entry(tab2, width=15, state='normal')
-txtadminl2 = Entry(tab2, width=15, state='normal')
-
-txtadminc1 = Entry(tab2, width=15, state='normal')
-txtadminc2 = Entry(tab2, width=15, state='normal')
-
-spinadmint1 = Spinbox(tab2, from_=15, to=40, width=5)
+canvas.pack()
 
 
-lbladmin1.grid(column=0, row=0)
+##-- Dashboard
 
-lbladminl1.grid(column=0, row=1)
-lbladminl2.grid(column=0, row=2)
-txtadminl1.grid(column=1, row=2)
-lbladminl3.grid(column=0, row=3)
-txtadminl2.grid(column=1, row=3)
+##--- Header
+header = tk.Frame(app, bg=BGFRAME, bd=5)
+header.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.1)
 
-lbladminc1.grid(column=0, row=4)
-lbladminc2.grid(column=0, row=5)
-txtadminc1.grid(column=1, row=5)
-lbladminc3.grid(column=0, row=6)
-txtadminc2.grid(column=1, row=6)
+title = tk.Label(header,anchor='center', text="RaspberryAQ-controller", bg=BGFRAME, bd=0, font=H1)
+title.place(relwidth=1, relheight=1)
 
-lbladmint1.grid(column=0, row=7)
-lbladmint2.grid(column=0, row=8)
-spinadmint1.grid(column=1, row=8)
+##--- Values
 
-# -- Buttons
+frameval = tk.Frame(app, bg=BGFRAME, bd=5)
+frameval.place(relx=0.1, rely=0.2, relwidth=0.8, relheight=0.6)
 
-lblsubmit = Label(tab2, text=" ", font=("Arial", 10))
-lblsubmit.grid(column=1, row=10)
-
-btnadmin1 = Button(tab2, text="Config File Schreiben", bg="gray", fg="White", command=clickedadmin)  # --Definiert ein Button
-btnadmin1.grid(column=1, row=11)  # --Definiert die Position des Buttons
-
-btnadmin2 = Button(tab2, text="Cancel", bg="gray", fg="White", command=clickecancel)  # --Definiert ein Button
-btnadmin2.grid(column=2, row=11)  # --Definiert die Position des Buttons
+title = tk.Label(header,anchor='center', text="RaspberryAQ-controller", bg=BGFRAME, bd=0, font=H1)
+title.place(relwidth=1, relheight=1)
 
 
-window.mainloop()
+##--- Buttons
+
+framebtn = tk.Frame(app, bg=BGFRAME, bd=5)
+framebtn.place(relx=0.1, rely=0.85, relwidth=0.8, relheight=0.1)
+
+btnadmin = tk.Button(framebtn, text="Configure", bg="gray", fg="White", font=H2, command=clickedadmin)  # --Definiert ein Button
+btnadmin.place(relwidth=0.49, relheight=1)  # --Definiert die Position des Buttons
+
+btncancel = tk.Button(framebtn, text="Cancel", bg="gray", fg="White", font=H2, command=clickecancel)  # --Definiert ein Button
+btncancel.place(relx=0.5, relwidth=0.49, relheight=1)  # --Definiert die Position des Buttons
+
+
+app.mainloop()
